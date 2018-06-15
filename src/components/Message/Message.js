f3784e9964bc79285a161be5046052b19328edeb
Franco Cuarterolo <fcuarterolo@gmail.com>
2018-06-15 16:56:06 -0300

    Add table component and styles

--- src/components/Message/Message.js
+++ src/components/Message/Message.js
@@ -2,7 +2,8 @@ import React from 'react';
 import './styles.scss'
 
 const Message = (props) => {
-    return <p>Hello {props.user.name} {props.user.surname} from {props.user.country}, on {props.user.bday} of {props.user.bmonth} you will have {props.user.age}</p>
+    const monthNames = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+    return <p className="message">Hello {props.user.name} {props.user.surname} from {props.user.country}, on {props.user.bday} of {monthNames[props.user.bmonth-1]} you will have {props.user.age}</p>
 };
 
 export default Message
---
+++ src/components/Table/Table.js
@@ -0,0 +1,51 @@
+import React, { Component } from 'react';
+import './styles.scss';
+
+ class Table extends Component {
+    
+    constructor(props){
+        super(props);
+        this.state = {
+            users: [],
+        };
+    }
+ 
+    componentDidUpdate(prevProps){
+        if(prevProps.user !== this.props.user ){
+            this.setState({
+                users: [
+                    ...this.state.users,
+                    this.props.user
+                ]
+            })
+        }    
+    }
+
+    render(){
+        let rows = this.state.users.map((user, index) => {
+            return (
+            <tr key={index}>
+                <td>{user.name} {user.surname}</td>
+                <td>{user.country}</td>
+                <td>{user.birthday}</td>
+            </tr>)
+        });
+        return(
+            <table>
+                <thead>
+                    <tr>
+                        <th>Name</th>
+                        <th>Country</th>
+                        <th>Birthday</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {rows}
+                </tbody>
+            </table>
+        );
+    }
+ }
+
+
+ export default Table;
